85b5e6ba85221f486973e52e12d882c36d1aa348
Walter Angolar <walter.angolar@tistech.co.ao>
2022-08-09 10:27:02 +0100

    feat: change product description

--- tests/api/product.test.js
+++ tests/api/product.test.js
@@ -1,7 +1,7 @@
 import Product from "../../src/models/product.js"
 
 test('verificar se a app é geopeças', () => {
-    const product = new Product('Geopeças', 'E-commerce de peças')
+    const product = new Product('Geopeças', 'E-commerce de peças usadas')
 
     expect(product.name).toBe('Geopeças')
 })
